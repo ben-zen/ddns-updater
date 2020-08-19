2df226c2e572ae671863f549ef1545d10546b744
Ben Lewis <1884340+ben-zen@users.noreply.github.com>
2020-08-19 15:01:54 -0700

    Rewrote to use JSON, trying more config stuff
    I'm getting the tool to do more debuggy-type stuff, like having a what-if
    option before actually updating a bunch of records.
    
    I really want the tool to accept multiple files, but that's not gonna work with
    the `config` crate, looks like it's time to parse some files manually. Sigh.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,57 @@
 mod settings;
 mod updater;
 
+use std::env;
+
+enum Action {
+  NoOp, // -h results in NoOp
+  Update,
+  WhatIf // -whatif results in printing actions but not updating
+}
+
+enum Options {
+  Folder(String),
+  Verbose
+}
+
+fn read_arguments () -> (Action, Vec<Options>) {
+  // do I do this lispy or not?
+
+  let mut action = Action::Update;
+  for arg in env::args() {
+    if arg.eq("-h") {
+      action = Action::NoOp;
+    }
+    if arg.eq("-whatif") {
+      action = Action::WhatIf;
+    }
+  }
+  (action, vec![])
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let client = reqwest::Client::new();
-
   let conf = settings::Settings::new()?;
-  updater::update_ddns_record(&client,
-                              &conf.dns_record.host,
-                              &conf.dns_record.key).await?;
-  
-  Ok(())
+
+  let (action, options) = read_arguments();
+  // find a "what if" option and print the actions that would be taken
+
+  match action {
+    Action::NoOp => { Ok(()) }
+    Action::Update => {
+      let client = reqwest::Client::new();
+      for dns_record in conf.dns_records {
+        updater::update_ddns_record(&client,
+                                    &dns_record.host,
+                                    &dns_record.key).await?;
+      }
+      Ok(())
+    }
+    Action::WhatIf => {
+      for dns_record in conf.dns_records {
+        println!("Update A record for {:?}", dns_record.host);
+      }
+      Ok(())
+    }
+  }
 }
--- src/settings.rs
+++ src/settings.rs
@@ -10,15 +10,20 @@ pub struct DNSRecord {
 #[derive(Debug, Deserialize)]
 pub struct Settings {
   pub debug: bool,
-  pub dns_record:  DNSRecord
+//  #[serde(rename="dns_record")]  
+  pub dns_records: Vec<DNSRecord>
 }
 
 impl Settings {
   pub fn new() -> Result<Self, ConfigError> {
     let mut s = Config::new();
     s.merge(File::with_name("test"))?;
+    println!("{:#?}", s);
+    s.merge(File::with_name("test1"))?;
 
     s.set("debug", false)?;
+
+    println!("{:#?}", s);
 
     s.try_into()
   }
